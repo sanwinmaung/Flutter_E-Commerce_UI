45717fcde11c74a54bb2ce97df34e03ea833887e
sanwinmaung <sanwinmaung.dev@gmail.com>
2020-12-14 02:09:39 +0630

    E-Commerce UI

---
+++ lib/constant/data.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+List menus = [
+  'Decoration', 'Lighting', 'Storage', 'Living Room'
+];
+
+List items = [
+  {
+    'id' : 1,
+    'name' : 'F01',
+    'code' : 'SWM08H001',
+    'img' : 'assets/images/f_01.jpg',
+    'p_price' : 100.00,
+    'p_promotion_price' : 55.00
+  },
+  {
+    'id' : 2,
+    'name' : 'F02',
+    'code' : 'SWM08H002',
+    'img' : 'assets/images/f_02.jpg',
+    'p_price' : 120.00,
+    'p_promotion_price' : 75.00
+  },
+  {
+    'id' : 3,
+    'name' : 'F03',
+    'code' : 'SWM08H003',
+    'img' : 'assets/images/f_03.jpg',
+    'p_price' : 170.00,
+    'p_promotion_price' : 80.00
+  },
+  {
+    'id' : 4,
+    'name' : 'F04',
+    'code' : 'SWM08H004',
+    'img' : 'assets/images/f_04.jpg',
+    'p_price' : 125.00,
+    'p_promotion_price' : 70.00
+  },
+  {
+    'id' : 5,
+    'name' : 'F05',
+    'code' : 'SWM08H005',
+    'img' : 'assets/images/f_05.jpg',
+    'p_price' : 115.00,
+    'p_promotion_price' : 50.00
+  },
+  {
+    'id' : 6,
+    'name' : 'F06',
+    'code' : 'SWM08H006',
+    'img' : 'assets/images/f_06.jpg',
+    'p_price' : 60.00,
+    'p_promotion_price' : 33.00
+  },
+];
+
+List colors = [
+  Colors.redAccent,
+  Colors.greenAccent,
+  Colors.cyanAccent,
+  Colors.orangeAccent,
+  Colors.purpleAccent,
+  Colors.blueAccent,
+];
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:ecom_one/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
